d94691a106796702f89e00dafce0e8f00b25533b
Andr3wBro <andr3wbro@gmail.com>
2022-07-05 13:34:22 +0300

    hm done

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+void CheckPow(int A, int B)
+{
+
+    if (A > B)
+    {
+        return;
+    }
+    if (A % 2 == 0) Console.Write($"{A}; ");
+    CheckPow(A + 1, B);
+
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int A= Prompt("Enter A =>");
+int B= Prompt("Enter B =>");
+CheckPow(A, B);
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int NaturalSum(int A, int B)
+{
+    if(A>B)
+    {
+        return 0;
+    }
+    int sum =0;
+    if(A>0 && B>0) // Проверка на натуральные число.
+    {
+        sum+=A;
+    }
+   return NaturalSum(A+1,B)+sum;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int A= Prompt("Enter A =>");
+int B= Prompt("Enter B =>");
+System.Console.WriteLine(NaturalSum(A,B));
